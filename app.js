29e62061d47630ab26c26b7082bb62c23661e288
Prashanth Nallathamby <prashanth.baskaran101@gmail.com>
2023-11-02 14:34:28 -0400

    new express generator

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
+    //const expressLayouts = require('express-ejs-layouts')
 const app = express()
-const port = 3000
+const port = 5000
 
 
 app.use(express.static('public'))
@@ -9,6 +10,7 @@ app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/media', express.static(__dirname + 'public/media'))
 
 
+//app.use(expressLayouts)
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
